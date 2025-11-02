60b14ed09ac6f53cd74f64fc21b57d31bf720279
soumen321 <bhattacharjee.soumen@gmail.com>
2025-11-02 07:43:34 +0530

    feat: ollama demo

---
+++ Langchain/ollama/app.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.llms import Ollama
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+load_dotenv()
+
+#Langsmith tracking
+os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
+os.environ['LANGCHAIN_TRACING_V2'] = 'true'
+os.environ["LANGCHAIN_PROJECT"] = os.getenv('LANGCHAIN_PROJECT')
+
+
+## prompt template
+prompt=ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant that helps people find information."),
+    ("human", "{question}") 
+])
+
+## streamlit framework
+st.title("Langchain Demo wth llama2")
+input_text = st.text_input("What question you have in mind")
+
+
+## Ollama llama2 LLM
+llm = Ollama(model="llama3.2:1b")
+output_parser = StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
+    
